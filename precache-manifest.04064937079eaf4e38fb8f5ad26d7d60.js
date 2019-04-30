6f453f0236e8d555d0ec814a03acd235a25fd5ac
Daniel Tian <danieltian31@yahoo.com>
2019-04-29 21:25:33 -0500

    Updates

---
+++ precache-manifest.04064937079eaf4e38fb8f5ad26d7d60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d821c3dd42fb44423f32d3d3ebfa7f",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "ef870d58c48343b3e15b",
+    "url": "/clicky-game/static/css/main.90acb412.chunk.css"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/clicky-game/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "ef870d58c48343b3e15b",
+    "url": "/clicky-game/static/js/main.3022aa78.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
