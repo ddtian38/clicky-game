fa37df15ce9680e855dab58a96e5b86c758e87f8
Daniel Tian <danieltian31@yahoo.com>
2019-04-30 09:07:30 -0500

    deployed successfully to gh-pages

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -4,7 +4,7 @@ import "./header.css"
 const Header = (props) => {
     return <nav>
     <div className="nav-wrapper">
-      <a href="/" className="brand-logo left">Avenger Memory Game</a>
+      <a href="#/" className="brand-logo left">Avenger Memory Game</a>
       <span className = "brand-logo center">Click on an image to begin</span>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li>Score: <span>{props.currentScore}</span></li>
